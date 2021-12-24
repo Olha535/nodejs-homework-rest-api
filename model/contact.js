d57653189e7a2f2baac7095ae8d3b99dc5c2c774
Olha Tarasova <tarasovao0505@gmail.com>
2021-12-24 10:54:14 +0200

    Connection to MongoDB using Mongoose, added PATCH

---
+++ model/contact.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("Joi");
+
+const contactSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const joiSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.bool,
+}).min(1);
+
+const Contact = model("contact", contactSchema);
+
+module.exports = { Contact, joiSchema };
